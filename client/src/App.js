8df37d552ad7b70797ce406ca033679940621ab9
MeladAbulail <melad.k.abulail@gmail.com>
2023-11-04 21:50:30 +0300

    frommelad

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Signin from './Components/Registration/Signin';
+
+import Footer from './Components/Footer';
 import './App.css';
+import Aboutusintro from './Components/Aboutus/Aboutusintro';
+import Contactform from './Components/Contactus/Contactform';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Contactform/>
     </div>
   );
 }
