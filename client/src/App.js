82179d05b100dfbf11f0d6fe6de477aeb145cbd8
b-ayan <bayanalkhadee@gmail.com>
2023-11-04 22:16:24 +0300

    adding bayan's files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import ProductList from "./Cards";
+import TopRated from "./TopRated";
+import Categories from "./Categories";
+import CeramicWorkshopDetails from "./Workshop";
+import Catalog from "./Catalog";
+import { AuthProvider } from "./AuthContext";
+import Modal from "./LoginModal";
+import { useState } from "react";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Nav />
+        </div>
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+
+        <Routes>
+          <Route exact path="/" element={<Home openModal={openModal} />} />
+          <Route exact path="/catalog" element={<Catalog />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
